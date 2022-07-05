4ab9f7675193ad623401b2d68ae13df5b81df3da
ronitnag04 <115322502+ronitnag04@users.noreply.github.com>
2022-07-05 03:31:21 +0000

    Sync LeetCode submission - Remove Element (python)

---
+++ problems/remove_element/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
+        index = 0
+        while index < len(nums):
+            if nums[index] == val:
+                nums.pop(index)
+            else:
+                index += 1
+        
