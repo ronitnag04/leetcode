0ec9c73e2e18343d4b59508158c2656edca71e33
ronitnag04 <115322502+ronitnag04@users.noreply.github.com>
2022-07-18 22:53:00 +0000

    Sync LeetCode submission - Array of Doubled Pairs (python3)

--- problems/array_of_doubled_pairs/solution.py
+++ problems/array_of_doubled_pairs/solution.py
@@ -1,11 +1,78 @@
 class Solution(object):
-    def canReorderDoubled(self, A):
-        count = collections.Counter(A)
-        for x in sorted(A, key = abs):
-            if count[x] == 0: continue
-            if count[2*x] == 0: return False
-            count[x] -= 1
-            count[2*x] -= 1
-
-        return True
+    def canReorderDoubled(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
         
+        """ V1 Not Time Efficient
+        matches = {}
+        
+        for num in arr:
+            if num not in matches.keys():
+                matches[num] = 1
+            else:
+                matches[num] += 1
+        
+        arr.sort()
+        if 0 in matches.keys():
+            if matches[0] % 2 != 0:
+                return False
+            else:
+                del matches[0]
+                while 0 in arr:
+                    arr.pop(arr.index(0))
+        
+        while len(arr) > 1:
+            temp = arr.pop(0)
+            matches[temp] -= 1
+            if temp * 2 in matches.keys() and matches[temp*2] > 0:
+                nTemp = arr.pop(arr.index(temp*2)) 
+                matches[nTemp] -= 1
+            elif temp / 2 in matches.keys() and matches[temp/2] > 0:
+                
+                nTemp = arr.pop(arr.index(temp/2)) 
+                matches[nTemp] -= 1
+            else:
+                return False
+        return True
+        """
+        
+        matches = {}
+        
+        for num in arr:
+            if num not in matches.keys():
+                matches[num] = 1
+            else:
+                matches[num] += 1
+        
+        
+        if 0 in matches.keys():
+            if matches[0] % 2 != 0:
+                return False
+            else:
+                del matches[0]
+        
+        while matches:
+            temp = min(matches.keys())
+            if temp * 2 in matches.keys() and matches[temp*2] > 0:
+                if matches[temp] == 1:
+                    del matches[temp]
+                else:
+                    matches[temp] -= 1
+                if matches[temp*2] == 1:
+                    del matches[temp*2]
+                else:
+                    matches[temp*2] -= 1
+            elif temp / 2 in matches.keys() and matches[temp/2] > 0:
+                if matches[temp] == 1:
+                    del matches[temp]
+                else:
+                    matches[temp] -= 1
+                if matches[temp/2] == 1:
+                    del matches[temp/2]
+                else:
+                    matches[temp/2] -= 1
+            else:
+                return False
+        return True
