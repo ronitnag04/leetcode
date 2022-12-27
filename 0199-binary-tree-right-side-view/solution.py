0447d92e1e87e1c6583bf955477510d2d880b683
ronitnag04 <115322502+ronitnag04@users.noreply.github.com>
2022-12-27 02:23:15 -0600

    Time: 30 ms (95.54%), Space: 13.8 MB (71.34%) - Migrate Sync

---
+++ 0199-binary-tree-right-side-view/solution.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        right = self.rightSideView(root.right)
+        left = self.rightSideView(root.left)
+        if len(left) > len(right):
+            right.extend(left[len(right):])
+        return [root.val] + right
+        
